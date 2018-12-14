66b585da754172bafcf555d8a60466e03de55c3a
Bubblemelon <12985181+Bubblemelon@users.noreply.github.com>
2018-12-14 09:44:00 -0800

    rename parameters of onUpgrade

--- app/src/main/java/com/cherylfong/airlineapp/AppDBHelper.java
+++ app/src/main/java/com/cherylfong/airlineapp/AppDBHelper.java
@@ -48,7 +48,7 @@ public class AppDBHelper extends SQLiteOpenHelper{
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1){
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion){
 
         Log.d("AppDBHelper", "onUpgrade()");
 
